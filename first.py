9aa3e81013f7d498184ed09a0a638db457260d0d
sauravsb99 <sauravsb99@gmail.com>
2019-05-10 19:10:02 +0530

    Game completed with One Enemy

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 import turtle
 import os
-
+import math
 # making a blank screen
 #
 scr = turtle.Screen()
@@ -45,6 +45,27 @@ enemy.speed(0)
 enemy.setposition(-250,250)
 
 
+#Making Infinity stones
+#
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5,0.5)
+bullet.hideturtle()
+# bullet.setposition(plr.xcor(),plr.ycor()+15)
+
+# Collision condition
+#
+def Collision(t1,t2):
+    z = math.sqrt(math.pow(t1.xcor() - t2.xcor(),2) + math.pow(t1.ycor() - t2.ycor(),2) )
+    if z < 20 :
+        return True
+    else :
+        return False
+    
 
 
 # Moving the player left
@@ -71,14 +92,31 @@ def move_right() :
     plr.setx(x)
 
 
+bullet_speed = 20
+bullet_status = "ready"
+
+# Bullet Movement
+#
+def fire() :
+    global bullet_status
+    if(bullet_status == "ready"): 
+        bullet_status = "fire"
+        x = plr.xcor()
+        y = plr.ycor() + 15
+        bullet.setposition(x,y)
+        bullet.showturtle()
+
+
+
 #Read input from keyboard
 #
 turtle.listen()
 turtle.onkey(move_left,"Left")
 turtle.onkey(move_right,"Right")
+turtle.onkey(fire,"space")
 
 # thanos movement
-enemy_speed = 2
+enemy_speed = 8
 
 while True :
 
@@ -93,8 +131,30 @@ while True :
         enemy_speed *= -1
 
     y = enemy.ycor()
-    y -= 0.1
+    y -= 0.4
     enemy.sety(y)
+
+#move the bullet
+#
+    if bullet_status == "fire" :
+        y = bullet.ycor()
+        y += bullet_speed
+        bullet.sety(y)
+
+    if bullet.ycor() > 275 :
+        bullet.hideturtle()
+        bullet_status = "ready"
+
+    if (Collision(bullet, enemy)):
+        enemy.setposition(-250 , 250)
+        bullet.hideturtle()
+        bullet_status="ready"
+
+    if (Collision(plr, enemy)):
+        plr.hideturtle()
+        enemy.hideturtle()
+        print("Game Over")
+        break
 
 
 
